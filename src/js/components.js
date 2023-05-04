843a37b3bedfcf62a09640c54bd3d0049fc3b0a0
Ehan A <ehan@ehanaha.me>
2023-05-04 13:26:29 +0000

    Update 'src/js/components.js'

--- src/js/components.js
+++ src/js/components.js
@@ -1,2 +1,13 @@
 
-document.getElementsByClassName("checkbox").classlist.toggle("select");
+var components = {
+    init: function () {
+        for (var i = 0; i < document.getElementsByClassName("checkbox").length; i++) {
+            document.getElementsByClassName("checkbox")[i].addEventListener(
+                "click",
+                function (event) {
+                    event.target.classlist.toggle("select");
+                }
+            );
+        }
+    }
+}
