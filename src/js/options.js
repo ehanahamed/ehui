b683fb1c02d565673e6da3ed14cc3f418b2889ab
Ehan A <ehan@ehanaha.me>
2023-06-02 18:01:09 +0000

    Add 'src/js/options.js'

---
+++ src/js/options.js
@@ -0,0 +1,41 @@
+/*!
+design (design.ehan.dev)
+Copyright (c) 2023-present Ehan Ahamed and contributors
+Licensed under the UPL-1.0 License
+https://src.ehan.dev/design/LICENSE.txt
+*/
+
+document.getElementById("headerMenuMenubutton").addEventListener("click", function () {
+    document.getElementById('headerMenuMenudiv').classList.toggle('hide');
+});
+document.getElementById("headerOptionsLangbutton").addEventListener("click", function () {
+    document.getElementById('headerOptionsLangdiv').classList.toggle('hide');
+    document.getElementById('headerOptionsThemediv').classList.add('hide');
+    document.getElementById('headerOptionsOptionsdiv').classList.add('hide');
+});
+document.getElementById("headerOptionsThemebutton").addEventListener("click", function () {
+    document.getElementById('headerOptionsLangdiv').classList.add('hide');
+    document.getElementById('headerOptionsThemediv').classList.toggle('hide');
+    document.getElementById('headerOptionsOptionsdiv').classList.add('hide');
+});
+document.getElementById("headerOptionsOptionsbutton").addEventListener("click", function () {
+    document.getElementById('headerOptionsLangdiv').classList.add('hide');
+    document.getElementById('headerOptionsThemediv').classList.add('hide');
+    document.getElementById('headerOptionsOptionsdiv').classList.toggle('hide');
+});
+document.getElementById("headerOptionsThemedivPurpleish").addEventListener("change", function (event) {
+    setTheme("purpleish");
+});
+document.getElementById("headerOptionsThemedivOwl").addEventListener("change", function (event) {
+    setTheme("owl");
+});
+document.getElementById("headerOptionsThemedivVampire").addEventListener("change", function (event) {
+    setTheme("vampire");
+});
+function setTheme(theme) {
+    document.documentElement.classList.remove("purpleish", "owl", "vampire");
+    document.documentElement.classList.add(theme);
+}
+document.getElementById("headerOptionsOptionsdivMin").addEventListener("change", function (event) {
+    document.documentElement.classList.toggle("min")
+});
