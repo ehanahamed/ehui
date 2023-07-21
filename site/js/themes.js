1987b78fb00fd0ab4cedc6c402374cc8bde5c366
Ehan A <ehan@ehanaha.me>
2023-07-21 03:45:55 +0000

    Add 'site/js/themes.js'

---
+++ site/js/themes.js
@@ -0,0 +1,11 @@
+function setTheme(theme) {
+    /* function to be used in this script and in other settings menus */
+    document.documentElement.classList.remove("purpleish", "owl", "cat");
+    if (theme) {
+        document.documentElement.classList.add(theme);
+        localStorage.setItem("theme", theme);
+    }
+}
+
+/* run function with localStorage to set to last used theme */
+setTheme(localStorage.getItem("theme"));
