349dafab8651dc84c45f56ebced5092fba82130c
Ehan A <ehan@ehanaha.me>
2023-07-24 02:04:55 +0000

    Update 'site/js/themes.js'

--- site/js/themes.js
+++ site/js/themes.js
@@ -1,6 +1,6 @@
 function setTheme(theme) {
     /* function to be used in this script and in other settings menus */
-    document.documentElement.classList.remove("purpleish", "owl", "blurple", "cat", "catdim");
+    document.documentElement.classList.remove("purpleish", "owl", "owldark", "blurple", "cat", "catdim");
     if (theme) {
         document.documentElement.classList.add(theme);
         localStorage.setItem("theme", theme);
