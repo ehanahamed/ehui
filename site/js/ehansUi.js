4c604b17b01a191d65bb3f1d05403975996a71cf
Ehan A <ehan@ehanaha.me>
2023-10-25 14:28:03 +0000

    Add site/js/ehansUi.js

---
+++ site/js/ehansUi.js
@@ -0,0 +1,11 @@
+/*
+  design.ehan.dev
+  Copyright (c) Ehan Ahamed and contributors
+  Licensed under the UPL-1.0 License
+  https://design.ehan.dev/LICENSE.txt
+*/
+
+var ehansUi = {
+    version: "v0.3.0",
+    
+}
