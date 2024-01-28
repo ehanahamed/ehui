b661b0811837bd4305f32824be390797407cfc2e
Ehan A <ehan@ehanaha.me>
2024-01-27 20:48:54 -0500

    moved setTheme into "main file"

--- site/js/ehansUi.js
+++ site/js/ehansUi.js
@@ -6,6 +6,26 @@
 */
 
 var ehansUi = {
-    version: "v0.3.0",
-    
+  version: "v0.4.0",
+  setTheme: function (theme) {
+    document.documentElement.classList.remove(
+      "auto",
+      "purpleish",
+      "snow",
+      "owl",
+      "blurple",
+      "cat",
+      "catdim"
+    );
+    document.documentElement.classList.add(theme);
+    /* save theme to localstorage */
+    localStorage.setItem("theme", theme);
+  }   
+}
+
+/* get theme from localstorage */
+if (localStorage.getItem("theme")) {
+  ehansUi.setTheme(localStorage.getItem("theme"));
+} else {
+  ehansUi.setTheme("auto")
 }
