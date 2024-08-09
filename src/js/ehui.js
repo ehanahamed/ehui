f9010ff45d4e718a3b6dce50acf377b9753b03b0
Ehan A <ehan@ehanaha.me>
2024-08-09 13:41:41 -0400

    update ehui themes js

--- src/js/ehui.js
+++ src/js/ehui.js
@@ -5,24 +5,30 @@
   https://ehui.ehan.dev/LICENSE.txt
 */
 
-var ehUi = {
-  version: "v0.4.0",
-  setTheme: function (theme) {
-    document.documentElement.classList.remove(
-      "auto",
-      "purpleish",
-      "light",
-      "darker"
-    );
-    document.documentElement.classList.add(theme);
-    /* save theme to localstorage */
-    localStorage.setItem("theme", theme);
-  }   
-}
-
-/* get theme from localstorage */
-if (localStorage.getItem("theme")) {
-  ehUi.setTheme(localStorage.getItem("theme"));
-} else {
-  ehUi.setTheme("auto")
+var ehui = {
+  loadCss: function (url) {
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = url;
+    document.getElementsByTagName("head")[0].appendChild(link);
+  },
+  themes: [],
+  theme: {
+    sync: function (current, path, ext, update) {
+      if (window.localStorage) {
+        var theme = localStorage.getItem("theme");
+        if (theme !== null && ehui.themes.includes(theme) && theme != current) {
+          ehui.loadCss(path + theme + ext);
+          if (update) {
+            update(theme);
+          }
+        }
+      }
+    },
+    set: function (theme) {
+      if (window.localStorage) {
+        localStorage.setItem("theme", theme)
+      }
+    }
+  },  
 }
