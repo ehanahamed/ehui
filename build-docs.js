30f5d0fd4902f8582e0f22440d751bb26587df3b
Ehan A <ehan@ehanaha.me>
2024-11-15 18:19:51 +0000

    based off of Quizfreely's build-docs.js, modified to output static html rendered by eta

---
+++ build-docs.js
@@ -0,0 +1,80 @@
+import path from "node:path";
+import { promises as fsPromises } from "node:fs";
+import Showdown from "showdown"
+import { Eta } from "eta";
+const markdown = new Showdown.Converter();
+
+const eta = new Eta({
+    views: path.join(import.meta.dir, "src", "views"),
+    tags: [
+      "<eta>",
+      "</eta>"
+    ],
+    varName: "data",
+    defaultExtension: ".html"
+})
+
+let srcDir = path.resolve(import.meta.dirname, "docs");
+let outputDir = path.join(import.meta.dirname, "site", "docs");
+let outputListFile = path.join(import.meta.dirname, "site", "docs-output-list.js")
+
+let srcFiles = [];
+let srcSubfolders = [];
+async function recursiveDir(dir) {
+    let dirEnts = await fsPromises.readdir(dir, {
+        withFileTypes: true
+    });
+    for (var i = 0; i < dirEnts.length; i++) {
+        if (dirEnts[i].isDirectory()) {
+            srcSubfolders.push(path.join(dir, dirEnts[i].name));
+            await recursiveDir(path.join(dir, dirEnts[i].name));
+        } else if (dirEnts[i].isFile()) {
+            srcFiles.push(path.join(dir, dirEnts[i].name));
+        }
+    }
+};
+
+await recursiveDir(srcDir);
+await fsPromises.rm(outputDir, {
+    recursive: true,
+    force: true
+});
+await fsPromises.mkdir(outputDir);
+let outputFiles = [];
+let outputSubfolders = [];
+for (var i = 0; i < srcSubfolders.length; i++) {
+    let relativeFolderPath = path.relative(srcDir, srcSubfolders[i]);
+    let outputFolderPath = path.join(outputDir, relativeFolderPath);
+    await fsPromises.mkdir(outputFolderPath);
+    outputSubfolders.push(relativeFolderPath);
+}
+for (var i = 0; i < srcFiles.length; i++) {
+    if (srcFiles[i].endsWith(".md")) {
+        let relativeFilePath = path.relative(srcDir, srcFiles[i]);
+        /* remove `.md` (last 3 chars) & add `.html` ext */
+        let newRelativeFilePath = (relativeFilePath.substring(0, (relativeFilePath.length - 3)) + ".html");
+        let outputFilePath = path.join(outputDir, newRelativeFilePath);
+        let fileContent = await fsPromises.readFile(
+            srcFiles[i],
+            { encoding: "utf8" }
+        );
+        let contentHtml = markdown.makeHtml(fileContent);
+        let renderedHtml = eta.render("docs-page.html", {
+            content: contentHtml
+        })
+        await fsPromises.writeFile(
+            outputFilePath,
+            renderedHtml
+        )
+        outputFiles.push(newRelativeFilePath);
+        console.log("✅ " + relativeFilePath);
+    }
+}
+
+await fsPromises.writeFile(
+    outputListFile,
+    JSON.stringify({
+        files: outputFiles,
+        folders: outputSubfolders
+    })
+)
