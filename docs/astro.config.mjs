a38b96b1953201019e66bb8a6f31e8fe84b2a2a5
Ehan A <ehan@ehanaha.me>
2025-08-12 02:18:51 -0400

    fix base path

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    base: "/ehui/docs/"
 	integrations: [
 		starlight({
 			title: 'EhUI',
