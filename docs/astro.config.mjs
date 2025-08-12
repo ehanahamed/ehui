beb998d1609ff6079d40726deb7a896f259c3e48
Ehan <ehanahamed@ehan.dev>
2025-08-12 02:25:19 -0400

    fix syntax err

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    base: "/ehui/docs/"
+    base: "/ehui/docs/",
 	integrations: [
 		starlight({
 			title: 'EhUI',
